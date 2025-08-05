4d94ebc600fe6141902c36916086e6910c7048b7
admin <taeanstae248gmail.com>
2025-08-05 18:22:37 +0700

    part error

---
+++ service-ballthai/models/common.go
@@ -0,0 +1,31 @@
+package models
+
+// NationalityAPI represents the nationality data from the API
+type NationalityAPI struct {
+	Code     string `json:"code"`
+	FullName string `json:"full_name"` // API may use "name" or "full_name"
+	Name     string `json:"name"`      // Some APIs might use "name" instead of "full_name"
+}
+
+// ChannelAPI represents the channel data from the API
+type ChannelAPI struct {
+	Name string `json:"name"`
+	Logo string `json:"logo"`
+}
+
+// CountryAPI represents the country data from the API (used in Stadiums)
+type CountryAPI struct {
+	Name string `json:"name"`
+	Code string `json:"code"`
+}
+
+// ClubNameAPI represents club name data from the API (used in Stadiums)
+type ClubNameAPI struct {
+	TH string `json:"th"`
+	EN string `json:"en"`
+}
+
+// BaseDB represents common fields for database entities
+type BaseDB struct {
+	ID int
+}
