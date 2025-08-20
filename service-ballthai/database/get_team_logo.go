d320ed15a02f7f860a3655df9c803054bb47d569
admin <taeanstae248gmail.com>
2025-08-20 15:06:22 +0700

    update logo

--- service-ballthai/database/get_team_logo.go
+++ service-ballthai/database/get_team_logo.go
@@ -7,7 +7,7 @@ import (
 // GetTeamLogoByName returns the logo URL for a team by Thai name
 func GetTeamLogoByName(db *sql.DB, nameTh string) (string, error) {
 	var logo sql.NullString
-	err := db.QueryRow("SELECT logo_url FROM teams WHERE REPLACE(name_th, ' ', '') = REPLACE(?, ' ', '')", nameTh).Scan(&logo)
+		err := db.QueryRow(`SELECT logo_url FROM teams WHERE LOWER(REPLACE(name_th, ' ', '')) = LOWER(REPLACE(?, ' ', ''))`, nameTh).Scan(&logo)
 	if err != nil {
 		return "", err
 	}
